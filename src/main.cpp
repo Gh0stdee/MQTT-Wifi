c1851274c9855f06ca3360bd8707f7ea95313d84
unknown <thomaskuwaikwan@gmail.com>
2024-10-10 08:48:36 +0800

    added libraries and finished set-up

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ssid        //wi-fi ssid
+#define password      //wi-fi password
+#define mqtt_server "192.168.68.100"
+
+#define test_topic "test/number"
+
+WiFiClient espClient;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  client.setserver(mqtt_server, 1883);
+  //Looping until WiFi is connected
+  while (WiFi.status() != WL_CONNECTED)     //WL_CONNECTED IS A ENUM w/ VALUE = 3
+    delay(50);
+    Serial.print(".");
+
+  Serial.print("");
+  Serial.println("Wifi connected.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  
+  
+  }
+
